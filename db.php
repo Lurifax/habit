5a4d40470332c8b00bae48346e0843063263f870
Lurifax <stianalexanderolsen@gmail.com>
2018-11-17 15:21:51 +0100

    added gitignorelist

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
 <?php
 /* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'root';
-$db = 'habit';
+
+require 'secrets.php';
+
+$host = $hostHabit;
+$user = $userHabit;
+$pass = $passHabit;
+$db = $dbHabit;
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
