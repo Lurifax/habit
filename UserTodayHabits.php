904db474c6315386135f42169b877b9d859c3dae
Lurifax <stianalexanderolsen@gmail.com>
2018-11-19 12:53:30 +0100

    Added archivetable to keep track of progress of habits done by user. Also added message to user if they dont have habits to do at given day.

--- UserTodayHabits.php
+++ UserTodayHabits.php
@@ -6,15 +6,20 @@ require 'db.php';
 
 //Setter userid og sjekker med $resultat om det er registrerte habits på brukeren.
 $userId = $_SESSION['id'];
-$resultat = $mysqli->query("SELECT * FROM USERHABIT WHERE ID = $userId");
+$resultat = $mysqli->query("SELECT * FROM USERHABIT WHERE USERID = $userId");
+$day = strtolower(date('l'));
+$dagUtenHabit = $mysqli->query("SELECT * FROM USERHABIT WHERE USERID = $userId AND DAY != '$day'");
 
-
-
+// Sjekker her om det er noen habits registrert på brukeren, hvis ikke skrives det til bruker
 if ($resultat->num_rows == 0) {
-  echo "Ingen habits registrert";
-}
-else {
-  $day = date('l');
+  echo "Ingen habits registrert. Opprett habits for å se dine habits.";
+
+// Sjekker her om det er habits registrert på en annen dag enn idag, hvis ja skrives det ut til brukeren
+} elseif ($dagUtenHabit->num_rows > 0) {
+  echo "Du har ingen flere habits a utfore for idag.";
+
+// Hvis det er registrert habits for dagen idag skrives tabell med habits ut
+}else {
   $dagensHabits = $mysqli->query("SELECT userhabit.habitid, userhabit.day, userhabit.isDone, habit.id, habit.name from userhabit inner JOIN habit on userhabit.habitid=habit.id where userhabit.day='$day' and userhabit.userid = $userId") or die ($mysqli->error());
   echo "<h2>Dagens habits</h2>";
   echo "<table border='1'>
@@ -31,11 +36,12 @@ else {
   while($row = mysqli_fetch_array($dagensHabits)){
     $isDone = $row['isDone'];
     $habitId = $row['id'];
+    $habitDay = $row['day'];
     echo "<form action='updateHabit.php' method='post' name='updateHabit'>";
     echo "<tr>";
     echo "<td>" . $habitId . "</td>";
     echo "<td>" . $row['name'] . "</td>";
-    echo "<td>" . $row['day'] . "</td>";
+    echo "<td>" . $habitDay . "</td>";
     // Endrer her verdiene som lagres i databasen (0,1) til mer forståelige statuser
     switch ($isDone) {
       case "0":
@@ -49,6 +55,7 @@ else {
     echo "<td>" . $isDone . "</td>";
     echo "<td>" . "<input type='submit' value='Utfort'></input>" . "</td>";
     echo "<input type='hidden' value='$habitId' name='habitId'></input>";
+    echo "<input type='hidden' value='$habitDay' name='habitDay'></input>";
     echo "</tr>";
     echo "</form>";
 
@@ -56,7 +63,6 @@ else {
 
   echo "</table>";
 }
-
 
 
 ?>
--- updateHabit.php
+++ updateHabit.php
@@ -3,16 +3,22 @@
 session_start();
 require 'db.php';
 
-//Setter userid og sjekker med $resultat om det er registrerte habits på brukeren.
+// Setter userid og sjekker med $resultat om det er registrerte habits på brukeren.
 $userId = $_SESSION['id'];
 $habitId = $_POST['habitId'];
+$habitDay = $_POST['habitDay'];
 
+// Oppdaterer isDone i tabellen userhabit for den habitten brukeren har satt som 'utført'.
+// Inserter i tillegg et innslag av den aktuelle habiten i archiveuserhabit tabellen for lagring til bruk av statistikk og progresjon
 
 if (isset($_POST['habitId'])) {
   $updateHabitDone = $mysqli->query("UPDATE USERHABIT SET isDONE = 1 WHERE USERID = $userId AND HABITID = $habitId") or die ($mysqli->error());
+  $archiveHabitAsDone = $mysqli->query("INSERT INTO ARCHIVEUSERHABIT (USERID, HABITID, DAY) VALUES('$userId','$habitId','$habitDay')") or die ($mysqli->error());
   $mysqli->query($updateHabitDone);
+  $mysqli->query($archiveHabitAsDone);
   header("location: profile.php");
 }
+
 /*
 
 // Lager connection til basen
