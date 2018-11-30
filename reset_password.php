efe105d99a34f7989275728c18789da651e2409f
Lurifax <stianalexanderolsen@gmail.com>
2018-12-01 00:38:34 +0100

    .

--- reset_password.php
+++ reset_password.php
@@ -1,27 +1,28 @@
 <?php
-header('application/x-www-form-urlencoded');
+
 require_once('./includes/dbconnect.php');
 session_start();
-// Make sure the form is being submitted with method="post"
+
+// Sjekker at form benytter POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//if (isset($_POST['reset'])) {
+
 
         $nyttpassord = password_hash($_POST['nyttpassord'], PASSWORD_DEFAULT);
 
-        // We get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
+        // Henter epost, hash og id fra hidden inputfelt
         $epost = $connection->escape_string($_POST['epost']);
         $hash = $connection->escape_string($_POST['hash']);
         $id = $connection->escape_string($_POST['id']);
 
         $sql = "UPDATE user SET passord='$nyttpassord', hash='$hash' WHERE id='$id'";
 
-
-        if ($connection->query($sql) ) {
+        //Utfører her oppdatering av passord
+        if ($connection->update($sql) === TRUE){
         $_SESSION['melding'] = "Ditt passord er nå endret!";
         header("location: success.php");
-    }
-    else {
+      } else {
+        //Sender brukeren til error om request_method ikke er satt
         $_SESSION['melding'] = "Noe gikk galt!";
         header("location: error.php");
+      }
     }
-}
